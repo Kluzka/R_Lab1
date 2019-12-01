8a53061c9bb8feaffe841146c67c138430365f5c
Kluzka <ytworek@gmail.com>
2019-12-01 15:52:35 +0100

    Add files via upload

--- Heart.R
+++ Heart.R
@@ -1,24 +1,18 @@
-#
-#
-#
-#https://web.stanford.edu/~hastie/ElemStatLearn/datasets/
+# Wykrywanie Choroby Niedokrwiennej Serca (CHD)
 
+#install.packages("AMORE")
 library(AMORE)
 
-setwd("D:\\R") #Zmieniamy katalog roboczy na folder zawieraj¹cy dane
-#Wczytywanie danych ucz¹cych sieæ
-#Nag³owki w pliku glass.data zosta³y dodane rêcznie "IR,Na,Mg,Al,Si,K,Ca,Ba,Fe,Rodzaj"
-#Usuniêto pierwsz¹ kolumnê zawieraj¹c¹ indeksy
+#setwd("D:\\R") #Zmieniamy katalog roboczy na folder zawieraj?cy dane
 
-Heart = read.csv("SAHeart.csv")
+Heart = read.csv("~/R/R_Lab1/SAheart.csv")
+Heart
 
 Ilosc.Danych=nrow(Heart)
-set.seed(666)
-idxTren<-sample(1:Ilosc.Danych,2*Ilosc.Danych/3)  # pod³oga z 2/3 losowych indeksów do trenowania sieci
-idxTest<-setdiff(1:Ilosc.Danych,idxTren) #pozosta³e 1/3 indeksów
+set.seed(9)
+idxTren<-sample(1:Ilosc.Danych,2*Ilosc.Danych/3)
+idxTest<-setdiff(1:Ilosc.Danych,idxTren) 
 
-#definiujemy funkcjê zmieniaj¹c¹ zmienn¹ zawieraj¹c¹ nazwê etykiety 
-#na liczbê zmiennych binarnych, Ilosc.Danych jest wartoœci etykiet
 target<-function(x)
 {
   n<-length(x)
@@ -30,13 +24,14 @@ target<-function(x)
   colnames(T)<-wartosci
   return(T)
 }
-#zastosowanie powy¿szej funkcji dla danych okreœlaj¹cych etykiety
+
+#zastosowanie powy?szej funkcji dla danych okre?laj?cych etykiety
 wZadane<-target(Heart$chd)
 wZadane
 
-set.seed(777)
+set.seed(3)
 
-#tworzymy strukturê sieci
+#tworzymy struktur? sieci
 siec<-newff(n.neurons=c(9,4,2),
             learning.rate.global=0.02,
             momentum.global=0.8,
@@ -45,7 +40,7 @@ siec<-newff(n.neurons=c(9,4,2),
             method="ADAPTgdwm",
             error.criterium="LMS")
 
-#trenujemy sieæ
+#trenujemy sie?
 wynik<-train(siec,
              Heart[idxTren,-10],
              wZadane[idxTren,],
@@ -54,15 +49,21 @@ wynik<-train(siec,
              show.step=10,
              n.shows=1000)
 
-#wyœwietlam wartoœci b³êdów
-plot(wynik$Merror,type="l",xlab="Iteracja (x10)",
-     ylab="B³¹d", col="darkred")
+#wy?wietlam warto?ci b??d?w
+plot(wynik$Merror,
+     type="o",
+     xlab="Iteracja (x10)",
+     ylab="Blad", 
+     col="blue",
+     xlim = c(0,50)
+     )
 
-#stosujê wytrenowan¹ sieæ do danych testowych
+
+#stosuj? wytrenowan? sie? do danych testowych
 y<-sim(wynik$net,Heart[idxTest, -10])
-y
 
-#definiujê funkcjê oceny klasyfikacji (zamieniam liczby na etykietê)
+
+#definiuj? funkcj? oceny klasyfikacji (zamieniam liczby na etykiet?)
 test.klasyf<-function(zad,wy)
 {
   zadane<-max.col(zad)
@@ -71,7 +72,7 @@ test.klasyf<-function(zad,wy)
 }
 wynik<-test.klasyf(wZadane[idxTest,],y)
 
-#okreœlamy dok³adnoœæ klasyfikacji
-cat("Dok³adnoœæ klasyfikacji:",
+#okre?lamy dok?adno?? klasyfikacji
+cat("Dokladnosc klasyfikacji:",
     sum(diag(wynik))/sum(wynik)*100, "%\n")
 
